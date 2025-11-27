7482b1d442f906fbbd936dce30c0050a1ca31959
minsik0 <qwew1212@naver.com>
2025-11-28 03:59:04 +0900

    init commit

---
+++ notify-service/src/test/java/com/notify_service/NotifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notify_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
